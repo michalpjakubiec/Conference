9d805a47bdb14b7b82a40b155cc12e67a1258732
Michał Jakubiec <michalpjakubiec@gmail.com>
2018-04-10 23:04:42 +0200

    Add project files.

---
+++ Conference/Controllers/ConferenceController.cs
@@ -0,0 +1,47 @@
+﻿using Conference.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Conference.Controllers
+{
+    public class ConferenceController : Controller
+    {
+        static List<ConferenceUser> conferenceUserList = new List<ConferenceUser>();
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult AddUser()
+        {
+            return View(conferenceUserList);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken()]
+        public IActionResult AddUser(ConferenceUser conferenceUser)
+        {
+            using (var stream = new FileStream($"wwwroot/images/{conferenceUser.Avatar.FileName}", FileMode.Create))
+            {
+                conferenceUser.Avatar.CopyTo(stream);
+            }
+
+            conferenceUserList.Add(conferenceUser);
+
+            JsonSerializer serializer = new JsonSerializer { Formatting = Formatting.Indented };
+            serializer.Converters.Add(new JavaScriptDateTimeConverter());
+            serializer.NullValueHandling = NullValueHandling.Ignore;
+            using (StreamWriter sw = new StreamWriter(@"ConferenceUsers.json"))
+            {
+                serializer.Serialize(sw, conferenceUser);
+            }
+
+            return RedirectToAction("AddUser");
+        } 
+    }
+}
---
+++ Conference/Models/ConferenceUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Conference.Models
+{
+    public class ConferenceUser {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public IFormFile Avatar { get; set; }
+    }
+}
